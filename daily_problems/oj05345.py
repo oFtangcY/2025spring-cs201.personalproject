09be6ba23dd55f4de7be3abf3edef0a946b16047
Chenyu Tang <149170662+oFtangcY@users.noreply.github.com>
2025-02-05 00:15:25 +0800

    Create oj05345.py

---
+++ daily_problems/oj05345.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+
+for _ in range(m):
+    op, x = input().split()
+    x = int(x)
+
+    if op == 'C':
+        for i in range(n):
+            nums[i] = (nums[i] + x) % 65535
+
+    elif op == 'Q':
+        res = 0
+
+        for i in range(n):
+            bin_num = str(bin(nums[i]))[2:]
+
+            if x < len(bin_num) and bin_num[- x - 1] == '1':
+                res += 1
+
+        print(res)
