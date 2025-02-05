be9b1a4b5207b35ba8d5011bee8c6af7722aa14d
Chenyu Tang <149170662+oFtangcY@users.noreply.github.com>
2025-02-05 22:04:18 +0800

    Create sy294.py

---
+++ daily_problems/sy294.py
@@ -0,0 +1,22 @@
+n = int(input())
+pop_list = list(map(int, input().split()))
+s = []
+j = 0
+
+for i in range(1, n + 1):
+    s.append(i)
+    while s:
+        if pop_list[j] == s[-1]:
+            s.pop()
+            j += 1
+        else:
+            break
+
+    if j == n:
+        print('Yes')
+        exit()
+    elif len(s) == 0:
+        continue
+    elif pop_list[j] < s[-1]:
+        print('No')
+        exit()
