7a665e633b7c2da56bab65a5dd5c441c0c28ce7e
Chenyu Tang <149170662+oFtangcY@users.noreply.github.com>
2025-02-05 14:12:29 +0800

    Create LC234.py

---
+++ daily_problems/LC234.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        vals = []
+        curr_node = head
+
+        while curr_node is not None:
+            vals.append(curr_node.val)
+            curr_node = curr_node.next
+
+        return vals == vals[::-1]
