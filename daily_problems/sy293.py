ed48a941cde2a376b8e3189ad9ccf9fc6ef8a4e0
Chenyu Tang <149170662+oFtangcY@users.noreply.github.com>
2025-02-05 16:35:04 +0800

    Create sy293.py

---
+++ daily_problems/sy293.py
@@ -0,0 +1,14 @@
+s = []
+n = int(input())
+
+for _ in range(n):
+    order = input()
+
+    if order == 'pop':
+        if s:
+            print(s.pop())
+        else:
+            print(-1)
+    else:
+        order = order.split()
+        s.append(int(order[1]))
