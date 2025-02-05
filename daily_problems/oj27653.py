3098707dd87b978c942c1b3fb3353078c5398ccf
Chenyu Tang <149170662+oFtangcY@users.noreply.github.com>
2025-02-05 21:47:23 +0800

    Create oj27653.py

---
+++ daily_problems/oj27653.py
@@ -0,0 +1,17 @@
+from math import gcd
+
+
+class Fraction:
+    def __init__(self, num, den):
+        self.num = num
+        self.den = den
+
+    def __add__(self, other):
+        newnum = self.num * other.den + self.den * other.num
+        newden = self.den * other.den
+        common = gcd(newnum, newden)
+        return str(newnum // common) + '/' + str(newden // common)
+
+num1, den1, num2, den2 = map(int, input().split())
+fraction1, fraction2 = Fraction(num1, den1), Fraction(num2, den2)
+print(fraction1.__add__(fraction2))
