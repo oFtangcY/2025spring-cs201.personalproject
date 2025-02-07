25993ddd28602a5d562a1b6fcaf58cf1f8e18654
Chenyu Tang <149170662+oFtangcY@users.noreply.github.com>
2025-02-07 22:05:09 +0800

    Create oj20.py

---
+++ daily_problems/oj20.py
@@ -0,0 +1,91 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data, self.next = data, next
+
+class LinkList:  # 循环链表
+    def __init__(self):
+        self.tail = None
+        self.size = 0
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def pushFront(self, data):
+        nd = Node(data)
+        if self.tail == None:
+            self.tail = nd
+            nd.next = self.tail
+        else:
+            nd.next = self.tail.next
+            self.tail.next = nd
+        self.size += 1
+
+    def pushBack(self, data):
+        self.pushFront(data)
+        self.tail = self.tail.next
+
+    def popFront(self):
+        if self.size == 0:
+            return None
+        else:
+            nd = self.tail.next
+            self.size -= 1
+            if self.size == 0:
+                self.tail = None
+            else:
+                self.tail.next = nd.next
+            return nd.data
+
+    def printList(self):
+        if self.size > 0:
+            ptr = self.tail.next
+            while True:
+                print(ptr.data, end=" ")
+                if ptr == self.tail:
+                    break
+                ptr = ptr.next
+            print("")
+
+    def remove(self, data):
+        if self.size == 0:
+            return "EMPTY"
+
+        prev = self.tail
+        current = self.tail.next
+
+        while True:
+            if current.data == data:
+                if self.size == 1:
+                    self.tail = None
+                else:
+                    prev.next = current.next
+                    if current == self.tail:
+                        self.tail = prev
+                self.size -= 1
+                return True
+
+            if current == self.tail:
+                break
+
+            prev = current
+            current = current.next
+
+        return False
+
+
+t = int(input())
+for i in range(t):
+    lst = list(map(int, input().split()))
+    lkList = LinkList()
+    for x in lst:
+        lkList.pushBack(x)
+    lst = list(map(int, input().split()))
+    for a in lst:
+        result = lkList.remove(a)
+        if result == True:
+            lkList.printList()
+        elif result == False:
+            print("NOT FOUND")
+        else:
+            print("EMPTY")
+    print("----------------")
