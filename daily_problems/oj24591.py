84b0b9c141f2f08c17a0b07af14a19a6e01baf76
chenyu tang <tangcy8264243698@gmail.com>
2025-03-25 15:24:38 +0800

    Update files

--- daily_problems/oj24591.py
+++ daily_problems/oj24591.py
@@ -1,2 +1,38 @@
-oprators = {'+', '-', '*', '/'}
+oprators = {'+':1, '-':1, '*':2, '/':2}
+
+def infix_to_postfix(expression):
+    s = []
+    postfix = []
+    num = ''
+
+    for char in expression:
+        if char.isdigit() or char == '.':
+            num += char
+        else:
+            if num:
+                postfix.append(num)
+                num = ''
+            if char in '+-*/':
+                while s and s[-1] in '+-*/' and oprators[s[-1]] >= oprators[char]:
+                    postfix.append(s.pop())
+                s.append(char)
+            elif char == '(':
+                s.append(char)
+            elif char == ')':
+                while s and s[-1] != '(':
+                    postfix.append(s.pop())
+                s.pop()
+
+    if num:
+        postfix.append(num)
+    while s:
+        postfix.append(s.pop())
+
+    return postfix
+
+n = int(input())
+for _ in range(n):
+    expr = input()
+    print(' '.join(map(str, infix_to_postfix(expr))))
+
 
